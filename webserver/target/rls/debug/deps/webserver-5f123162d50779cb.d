3724ff67b86125e6e9304ff4adae254df1721926
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2022-05-02 09:34:53 +0100

    rm redundant folder

--- webserver/target/rls/debug/deps/webserver-5f123162d50779cb.d
+++
@@ -1,5 +0,0 @@
-/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-5f123162d50779cb.rmeta: src/main.rs
-
-/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-5f123162d50779cb.d: src/main.rs
-
-src/main.rs:
--- webserver/target/rls/debug/deps/webserver-8e4fcce654834b81.d
+++
@@ -1,5 +0,0 @@
-/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-8e4fcce654834b81.rmeta: src/main.rs
-
-/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-8e4fcce654834b81.d: src/main.rs
-
-src/main.rs:
