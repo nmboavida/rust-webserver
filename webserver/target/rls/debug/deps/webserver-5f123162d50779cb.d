c2e87e967731408ebeb6400bfb5e2265d8098221
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2022-05-02 09:31:21 +0100

    init cargo

---
+++ webserver/target/rls/debug/deps/webserver-5f123162d50779cb.d
@@ -0,0 +1,5 @@
+/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-5f123162d50779cb.rmeta: src/main.rs
+
+/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-5f123162d50779cb.d: src/main.rs
+
+src/main.rs:
---
+++ webserver/target/rls/debug/deps/webserver-8e4fcce654834b81.d
@@ -0,0 +1,5 @@
+/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-8e4fcce654834b81.rmeta: src/main.rs
+
+/Users/nuno/Documents/Programs/others/rust/webserver/webserver/target/rls/debug/deps/webserver-8e4fcce654834b81.d: src/main.rs
+
+src/main.rs:
