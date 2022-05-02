6aba01b48fe7ea7ba5098789eb845e1c9a17c0a6
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2022-05-02 19:44:23 +0100

    impl mpsc msg channel & execute method

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use std::thread;
+use std::sync::{mpsc, Mutex, Arc}; // multiple producer single consumer 
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -15,19 +19,26 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F) 
     where 
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
         // We still use the () after FnOnce because this FnOnce represents
         // a closure that takes no parameters and returns the unit type ()
     }
@@ -39,8 +50,14 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop { 
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job. Executing.", id);
+
+            job();
+         });
 
         Worker { id, thread }
     }
